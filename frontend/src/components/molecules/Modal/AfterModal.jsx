3bdc4d8f3ebe595c227f19edafbafc1195de2ddf
dantt2 <dantt1002@gmail.com>
2024-06-05 23:42:32 +0700

    fix minor

--- frontend/src/components/molecules/Modal/AfterModal.jsx
+++ frontend/src/components/molecules/Modal/AfterModal.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Button, Modal, Typography } from "antd";
 import checkIcon from "../../../assets/check-icon.png";
 
-const AfterModal = ({ open, handleCancel, handleOk, setOpen }) => {
+const AfterModal = ({
+  open,
+  handleCancel,
+  handleOk,
+  setOpen,
+  content = "Thành công",
+}) => {
   return (
     <Modal
       closable
@@ -23,9 +29,7 @@ const AfterModal = ({ open, handleCancel, handleOk, setOpen }) => {
         }}
       >
         <img style={{ width: "20%" }} src={checkIcon} />
-        <div style={{ fontSize: "30px", fontWeight: "600" }}>
-          Tạo đơn hàng thành công
-        </div>
+        <div style={{ fontSize: "30px", fontWeight: "600" }}>Thành công</div>
       </div>
     </Modal>
   );
